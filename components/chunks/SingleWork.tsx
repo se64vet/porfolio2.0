1fa8491a1b3416692463470d757c7bfa474b19f6
Lukas vm <lukasvm@Lukass-Laptop.local>
2023-11-30 21:24:58 -0500

    fix eslint bug

--- components/chunks/SingleWork.tsx
+++ components/chunks/SingleWork.tsx
@@ -21,33 +21,44 @@ const SingleWork = (props: WorkProps) => {
       <div
         className={`${
           props.even ? 'lg:flex-row-reverse ' : ''
-        } p-2 !justify-evenly flex-center-everything w-max flex-col-reverse gap-12 lg:w-full lg:flex-row`}
+        } flex-center-everything w-max flex-col-reverse !justify-evenly gap-12 p-2 lg:w-full lg:flex-row`}
       >
         <div
-			className={`lg:self-start ${
-            props.even ? 'text-right' : 'text-left'
-          }`}
+          className={`lg:self-start ${props.even ? 'text-right' : 'text-left'}`}
         >
-          <h2 className="text-5xl font-bold text-gray-600">{props.index}.{props.work.name}</h2>
+          <h2 className="text-5xl font-bold text-gray-600">
+            {props.index}.{props.work.name}
+          </h2>
           <p className="text-2xl">
             &#91;<span>{props.work.cat}</span>
             &#93; {props.work.type}
           </p>
           <div className="mt-1 leading-5">
             <Link href={props.work.demoUrl} passHref>
-              <a  href={props.work.demoUrl} target={"_blank"} className="relative cursor-pointer font-bold text-red-600  mr-3 text-2xl group">
-				Demo
-				  <span className="absolute -bottom-px left-0 w-0 h-0.5 bg-red-500 transition-all group-hover:w-full"></span> 
+              <a
+                href={props.work.demoUrl}
+                target={'_blank'}
+                className="group relative mr-3 cursor-pointer  text-2xl font-bold text-red-600"
+              >
+                Demo
+                <span className="absolute -bottom-px left-0 h-0.5 w-0 bg-red-500 transition-all group-hover:w-full"></span>
               </a>
             </Link>
 
-            <Link href={props.work.repoUrl}  passHref>
-			<a href={props.work.repoUrl} target={"_blank"} className="relative cursor-pointer font-bold text-gray-700 text-2xl group">
-				Code
-				  <span className="absolute -bottom-px right-0 w-0 h-0.5 bg-gray-700 transition-all group-hover:w-full"></span> 
-			  </a>
+            <Link href={props.work.repoUrl} passHref>
+              <a
+                href={props.work.repoUrl}
+                target={'_blank'}
+                className="group relative cursor-pointer text-2xl font-bold text-gray-700"
+              >
+                Code
+                <span className="absolute -bottom-px right-0 h-0.5 w-0 bg-gray-700 transition-all group-hover:w-full"></span>
+              </a>
             </Link>
-            <p className="mt-1" dangerouslySetInnerHTML={{ __html: props.work.desc_html }}></p>
+            <p
+              className="mt-1"
+              dangerouslySetInnerHTML={{ __html: props.work.desc_html }}
+            ></p>
           </div>
         </div>
 
@@ -56,11 +67,17 @@ const SingleWork = (props: WorkProps) => {
             style={{
               width: '30em',
               position: 'relative',
-			  aspectRatio: '1/1',
+              aspectRatio: '1/1',
             }}
-			  className="hover:drop-shadow-xl hover:-translate-y-1 ease-out duration-300"
+            className="duration-300 ease-out hover:-translate-y-1 hover:drop-shadow-xl"
           >
-            <Image className="rounded-md" layout="fill" objectFit="cover" src={props.work.imgUrl} />
+            <Image
+              className="rounded-md"
+              layout="fill"
+              objectFit="cover"
+              src={props.work.imgUrl}
+              alt="thumbnail"
+            />
           </div>
         </div>
       </div>
